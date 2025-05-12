fab79727dc63c17e0852cec640dc0493d16fb277
Wiktor Chomik <wiktor.chomik@sap.com>
2025-05-12 15:32:15 +0200

    chore: Removing .ci.config.yaml from tracked Chart files (#50)
    Co-authored-by: wchomik <wchomik@users.noreply.github.com>

--- typescript/packages/helm-chart/listing/dist/index.js
+++ typescript/packages/helm-chart/listing/dist/index.js
@@ -55405,7 +55405,7 @@ module.exports = {
 "use strict";
 
 Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.Msgs = exports.ErrorMsgs = exports.HelmChartDoc = exports.Yaml = exports.Functionality = exports.github = exports.envvars = exports.HelmChartFiles = exports.ListingYamlKeys = void 0;
+exports.Msgs = exports.ErrorMsgs = exports.HelmChartDoc = exports.Yaml = exports.Functionality = exports.github = exports.envvars = exports.versionBumpIgnoredFiles = exports.HelmChartFiles = exports.ListingYamlKeys = void 0;
 exports.ListingYamlKeys = {
     dir: 'dir',
     name: 'name',
@@ -55421,6 +55421,7 @@ exports.HelmChartFiles = {
     ReadmeMd: 'README.md',
     listingFile: 'helm-chart-listing.yaml'
 };
+exports.versionBumpIgnoredFiles = ['.ci.config.yaml'];
 exports.envvars = {
     SOURCE_GIT_REPO_URL: 'SOURCE_GIT_REPO_URL',
     TARGET_GIT_REPO_URL: 'TARGET_GIT_REPO_URL',
