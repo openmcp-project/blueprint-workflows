c3921a0c385d939703f97cf43d5102500ac93a00
wchomik <wchomik@users.noreply.github.com>
2025-05-30 12:12:33 +0000

    chore(ci): k8s manifest templated for Helm Charts

--- typescript/packages/k8s-manifest-templating/dist/index.js
+++ typescript/packages/k8s-manifest-templating/dist/index.js
@@ -66565,7 +66565,7 @@ async function run() {
             let listingYamlName = shared_1.utils.unrapYamlbyKey(yamlitem, shared_1.constants.ListingYamlKeys.name);
             core.debug('Listing YAML Name:' + listingYamlName);
             let listingYamlManifestPath = shared_1.utils.unrapYamlbyKey(yamlitem, shared_1.constants.ListingYamlKeys.manifestPath);
-            core.debug('Listing YAML Relative Path:' + listingYamlManifestPath);
+            core.debug('Listing YAML Manifest Path:' + listingYamlManifestPath);
             let listingYamlRelativePath = shared_1.utils.unrapYamlbyKey(yamlitem, shared_1.constants.ListingYamlKeys.relativePath);
             core.debug('Listing YAML Relative Path:' + listingYamlRelativePath);
             let dir = path.parse(listingYamlDir);
@@ -66575,7 +66575,7 @@ async function run() {
                 core.debug('helmTemplatingOptions: ' + JSON.stringify(helmTemplatingOptions));
                 let runHelmTemplating = function (prefix, valueFiles) {
                     core.debug('runHelmTemplating called with prefix:' + prefix + ' and valueFiles:' + valueFiles);
-                    let manifestTargetFolder = path.parse(GITHUB_WORKSPACE + '/manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlName);
+                    let manifestTargetFolder = path.parse(GITHUB_WORKSPACE + '/manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split('/').pop());
                     core.debug('Creating manifest target folder: ' + path.format(manifestTargetFolder));
                     fs.mkdirSync(path.format(manifestTargetFolder), { recursive: true });
                     core.debug('Created folder: ' + path.format(manifestTargetFolder));
@@ -66594,7 +66594,7 @@ async function run() {
                     });
                     core.debug('Calling utilsHelmChart.template with args: ' + valueArgs + ' and helmOptions: ' + helmOptions);
                     utilsHelmChart.template(dir, valueArgs, helmOptions);
-                    tableRows.push([listingYamlName, listingYamlRelativePath, item, '✅', 'manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlName]);
+                    tableRows.push([listingYamlName, listingYamlRelativePath, item, '✅', 'manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split('/').pop()]);
                 };
                 // Only call .toJSON() if helmTemplatingOptions is not false and has .toJSON
                 let helmTemplatingOptionsObj = helmTemplatingOptions;
--- typescript/packages/k8s-manifest-templating/src/main.ts
+++ typescript/packages/k8s-manifest-templating/src/main.ts
@@ -61,8 +61,10 @@ export async function run(): Promise<void> {
         core.debug('helmTemplatingOptions: ' + JSON.stringify(helmTemplatingOptions))
 
         let runHelmTemplating = function (prefix: string, valueFiles: string[]) {
-          core.debug('runHelmTemplating called with prefix:' + prefix + ' and valueFiles:'+ valueFiles)
-          let manifestTargetFolder: path.FormatInputPathObject = path.parse(GITHUB_WORKSPACE + '/manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split("/").pop())
+          core.debug('runHelmTemplating called with prefix:' + prefix + ' and valueFiles:' + valueFiles)
+          let manifestTargetFolder: path.FormatInputPathObject = path.parse(
+            GITHUB_WORKSPACE + '/manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split('/').pop()
+          )
           core.debug('Creating manifest target folder: ' + path.format(manifestTargetFolder))
 
           fs.mkdirSync(path.format(manifestTargetFolder), { recursive: true })
@@ -88,7 +90,7 @@ export async function run(): Promise<void> {
 
           core.debug('Calling utilsHelmChart.template with args: ' + valueArgs + ' and helmOptions: ' + helmOptions)
           utilsHelmChart.template(dir, valueArgs, helmOptions)
-          tableRows.push([listingYamlName, listingYamlRelativePath, item, '✅', 'manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split("/").pop()])
+          tableRows.push([listingYamlName, listingYamlRelativePath, item, '✅', 'manifests/' + listingYamlManifestPath + '/' + prefix + listingYamlRelativePath.split('/').pop()])
         }
 
         // Only call .toJSON() if helmTemplatingOptions is not false and has .toJSON
